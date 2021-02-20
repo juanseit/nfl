339f9e4b7c0b3d67d040a43c7c544c9a2090619a
Juan Iturvide <76930767+juanseit@users.noreply.github.com>
2021-02-19 21:25:53 -0300

    Update rbwr.R

--- Posições/rbwr.R
+++ Posições/rbwr.R
@@ -24,7 +24,7 @@ rus_pbp <- decode_player_ids(pbp_3)
 rus_pbp <- rus_pbp[!is.na(rusher_id)]
 rosters <- as.data.table(nflfastR::fast_scraper_roster(2020, pp = TRUE))
 roster_subset <- rosters[!is.na(gsis_id), .N, by = "season,gsis_id,team,position"]
-rus_pbp[roster_subset, recpos := i.position, on = .(rusher_id = gsis_id, season = season)]
+rus_pbp[roster_subset, ruspos := i.position, on = .(rusher_id = gsis_id, season = season)]
 
 
 view(rus_pbp)
@@ -32,7 +32,7 @@ view(rus_pbp)
 pbp_rbwr <- merge(rus_pbp, rbwr, by = c("posteam"))
 
 rb_3 <- pbp_rbwr %>%
-  filter(recpos %in% c('RB'), rush == 1, down <= 4, week <= 17, !is.na(epa), !is.na(down)) %>%
+  filter(ruspos %in% c('RB'), rush == 1, down <= 4, week <= 17, !is.na(epa), !is.na(down)) %>%
   group_by(name) %>%
   summarize(epa_per_rush = mean(epa), 
             rbwr = mean(rbwr), plays = n()) %>%
@@ -42,15 +42,15 @@ rb_3 <- pbp_rbwr %>%
 
 lm_rb <- rb_3 %>%
   ggplot(mapping = aes(x = rbwr, y = epa_per_rush)) +
-  geom_point(color = "#91c390", size =  4) +
-  stat_smooth(method = "lm", color = "#359fda") + 
+  geom_point(color = "white", fill = "red", size =  4) +
+  stat_smooth(method = "lm", se = FALSE, color = "white") + 
   geom_text(aes(0.68,0.1,label = "R^2 =  0.2415")) +
   labs(x = "Run Block Win Rate (%)",
        y = "EPA/Play",
        title = "Como as linhas ofensivas impactam o desempenho dos Running Backs.",
        subtitle = "Mínimo 100 corridas por RB.",
        caption = "Gráfico: @juanseit_ | Data by @nflfastR and ESPN.") +
-theme_539() +
+theme_theathletic() +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
 
